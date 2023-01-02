24d43d55e7b6779f347aace968fd9e3f9ef5d363
Joao Manoel <joaomanoel.ifg@gmail.com>
2023-01-02 16:01:33 -0300

    atualizando pip
    atualizando para a versão mais nova do pip

--- mkvenv.sh
+++ mkvenv.sh
@@ -3,5 +3,6 @@ set -e
 mkdir -p ${HOME}/.local/venv/
 virtualenv -p /usr/bin/python3.9 ${HOME}/.local/venv/sistec_download_automatico
 source ${HOME}/.local/venv/sistec_download_automatico/bin/activate
+pip install -U pip
 pip install -U -r ${PWD}/requirements.txt
 exit 0
